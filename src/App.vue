cdd6fe7e9f6907616405a59746c0af6c016e05d6
luojiyin <luojiyin@hotmail.com>
2022-05-05 10:43:34 +0800

    use url read local file fail

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <div class="center">
-    
-        <canvas id="c" ></canvas>
-     </div>
+      <canvas id="c"></canvas>
+    </div>
+    <div id="ctrlButton">
+      <input id="fileUpload" type="file" hidden />
+      <button id="addImage" @click="uploadImage()">Add Image</button>&nbsp;
+      <button id="delImage">Delete Image</button>
+    </div>
   </div>
 </template>
 
@@ -11,35 +15,51 @@
 import { fabric } from "fabric";
 
 export default {
-  name: 'App',
-  components: {
-    
-  },
-  data(){
+  name: "App",
+  components: {},
+  data() {
     return {
-      width : window.innerWidth,
-      height : window.innerHeight,
-    }
+      width: window.innerWidth,
+      height: window.innerHeight,
+      canvas: null,
+    };
   },
   methods: {
-    
     initCanvas() {
-      this.canvas = new fabric.Canvas('c');
-      this.canvas.setWidth(this.width/2);
-      this.canvas.setHeight(this.height/2);
-      this.canvas.backgroundColor = '#fff';
+      this.canvas = new fabric.Canvas("c");
+      this.canvas.setWidth(this.width / 2);
+      this.canvas.setHeight(this.height / 2);
+      this.canvas.backgroundColor = "#fff";
       this.canvas.renderAll();
-      console.log("this width",this.width);
-    console.log("this height",this.height);
+      console.log("this width", this.width);
+      console.log("this height", this.height);
+    },
+
+    uploadImage() {
+      document.getElementById("fileUpload").click();
+      const file = document.getElementById("fileUpload").files[0];
+
+      var pugImg = new Image();
+      pugImg.onload = function () {
+        var pug = new fabric.Image(pugImg, {
+          angle: 45,
+          width: 500,
+          height: 500,
+          left: 50,
+          top: 70,
+          scaleX: 0.25,
+          scaleY: 0.25,
+        });
+        this.canvas.add(pug);
+      };
+
+      pugImg.src = URL.createObjectURL(file);
     },
   },
   mounted() {
-      this.initCanvas();
-    
-    },
-    
-
-}
+    this.initCanvas();
+  },
+};
 </script>
 
 <style>
