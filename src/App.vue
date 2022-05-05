2bb6e043dc487f3cd783bad3d6bf09a1206c0195
luojiyin <luojiyin@hotmail.com>
2022-05-05 08:36:40 +0800

    feat: set canvas center

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="center">
+    
+        <canvas id="c" ></canvas>
+     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { fabric } from "fabric";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  methods: {
+    initCanvas() {
+      this.canvas = new fabric.Canvas('label-canvas');
+      this.canvas.setWidth(this.width/2);
+      this.canvas.setHeight(this.height/2);
+      this.canvas.backgroundColor = '#fff';
+      this.canvas.renderAll();
+    },
+    addText() {
+      this.canvas.add(new fabric.Text('Hello World', {
+        left: 100,
+        top: 100,
+        fontSize: 40,
+        fill: '#333'
+      }));
+      this.canvas.renderAll();
+    },
+    addImage() {
+      fabric.Image.fromURL('./assets/logo.png', (img) => {
+        img.set({
+          left: 100,
+          top: 100,
+          angle: 0,
+          padding: 10,
+          cornersize: 10
+        });
+        img.scale(0.5);
+        this.canvas.add(img);
+        this.canvas.renderAll();
+      });
+    },
+    addRect() {
+      this.canvas.add(new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: '#333',
+        width: 100,
+        height: 100
+      }));
+      this.canvas.renderAll();
+    },
+    addCircle() {
+      this.canvas.add(new fabric.Circle({
+        left: 100,
+        top: 100,
+        fill: '#333',
+        radius: 50
+      }));
+      this.canvas.renderAll();
+    },
   }
+    
+
 }
 </script>
 
@@ -24,5 +79,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
 }
 </style>
